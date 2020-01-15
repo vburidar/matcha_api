ff4851c57b42ef33f9e8522e3e6c1c86a3c5c6c9
Thomas BAILLY-SALINS <tbailly-@student.42.fr>
2020-01-15 20:41:33 +0100

    add required, date, birthdate and bool rules, see README for more

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Matcha
+
+## Request validator
+
+Each time you create a route, you will have to implement data validation for incoming HTTP request.
+
+First, import the list of rules, named rv:
+```js
+import { rv } from './middlewares/requestValidator';
+```
+
+Then, create a schema to compare with HTTP request, like this:
+```js
+const createUserSchema = {
+  body: {
+    login: [rv.required(), rv.string()],
+    password: [rv.required(), rv.string(), rv.password()],
+    email: [rv.required(), rv.string(), rv.email()],
+    birthdate: [rv.required(), rv.date(), rv.birthdate()],
+    validated: [rv.required(), rv.bool()],
+  },
+};
+```
+
+List of available rules:
+
+Rule Name | Description | Rule Priority
+--------- | ----------- | -------------
+required | | 1
+string | | 2
+password | At least 9 characters long, 1 uppercase, 1 lowercase, 1 digit | 3
+email | | 3
+date | | 2
+birthdate | Not under 18yo and over 80yo | 3
+bool | | 2
+
+The lower the priority, the sooner you should add it in rules array (cf example above)
--- app/middlewares/requestValidator.js
+++ app/middlewares/requestValidator.js
@@ -1,3 +1,5 @@
+import { DateTime } from 'luxon';
+
 function requestValidator(schema) {
   return (req, res, next) => {
     const requestData = req.body;
@@ -26,22 +28,85 @@ function RvException(id, description, propertyName) {
 }
 
 const rv = {
+  required() {
+    return (val, propertyName) => {
+      if (typeof val === 'undefined') {
+        throw new RvException('missing', 'Missing', propertyName);
+      }
+    };
+  },
   string() {
     return (val, propertyName) => {
-      if (typeof val !== 'string') throw new RvException('invalid-string', 'Invalid string', propertyName);
+      if (typeof val !== 'undefined') {
+        if (typeof val !== 'string') {
+          throw new RvException('invalid-string', 'Invalid string', propertyName);
+        }
+      }
     };
   },
   password() {
     return (val, propertyName) => {
-      if (val.length < 9) throw new RvException('invalid-password-length', 'Invalid password (min: 9)', propertyName);
-      if (val.match(/[0-9]/) === null) throw new RvException('invalid-password-digit', 'Invalid password (no digit)', propertyName);
-      if (val.match(/[A-Z]/) === null) throw new RvException('invalid-password-uppercase', 'Invalid password (no upper-case letter)', propertyName);
-      if (val.match(/[a-z]/) === null) throw new RvException('invalid-password-lowercase', 'Invalid password (no lower-case letter)', propertyName);
+      if (typeof val !== 'undefined') {
+        if (val.length < 9) {
+          throw new RvException('invalid-password-length', 'Invalid password (min: 9)', propertyName);
+        }
+        if (!/[0-9]/.test(val)) {
+          throw new RvException('invalid-password-digit', 'Invalid password (no digit)', propertyName);
+        }
+        if (!/[A-Z]/.test(val)) {
+          throw new RvException('invalid-password-uppercase', 'Invalid password (no upper-case letter)', propertyName);
+        }
+        if (!/[a-z]/.test(val)) {
+          throw new RvException('invalid-password-lowercase', 'Invalid password (no lower-case letter)', propertyName);
+        }
+      }
     };
   },
   email() {
     return (val, propertyName) => {
-      if (val.match(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/ig) === null) throw new RvException('Invalid email', propertyName);
+      if (typeof val !== 'undefined') {
+        if (!/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/ig.test(val)) {
+          throw new RvException('invalid-email', 'Invalid email', propertyName);
+        }
+      }
+    };
+  },
+  date() {
+    return (val, propertyName) => {
+      if (typeof val !== 'undefined') {
+        if (
+          !/^\d{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$/.test(val)
+          || !DateTime.fromISO(val).isValid
+        ) {
+          throw new RvException('invalid-date', 'Invalid date', propertyName);
+        }
+      }
+    };
+  },
+  birthdate() {
+    return (val, propertyName) => {
+      if (typeof val !== 'undefined') {
+        const now = DateTime.local();
+        const birthdate = DateTime.fromISO(val);
+
+        const diffInYears = now.diff(birthdate, 'years');
+
+        if (diffInYears.values.years < 18) {
+          throw new RvException('invalid-birthdate-under-18', 'Invalid birthdate (under 18)', propertyName);
+        }
+        if (diffInYears.values.years > 80) {
+          throw new RvException('invalid-birthdate-over-80', 'Invalid birthdate (over 80)', propertyName);
+        }
+      }
+    };
+  },
+  bool() {
+    return (val, propertyName) => {
+      if (typeof val !== 'undefined') {
+        if (typeof val !== 'boolean') {
+          throw new RvException('invalid-boolean', 'Invalid boolean', propertyName);
+        }
+      }
     };
   },
   // min: function(min) {
