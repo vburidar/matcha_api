83aafa64e7a26d3a2d46ded631cde6d2d0f97536
Thomas BAILLY-SALINS <tbailly-@student.42.fr>
2020-01-17 11:20:23 +0100

    add EmailService basics with loading at startup and corresponding config

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import config from './config';
 import loaders from './loaders';
+import EmailService from './services/email';
 
 async function startServer() {
   const app = express();
 
   await loaders(app);
+
+  EmailService.load();
 
   app.listen(config.port, () => {
     console.log(`Server started on port ${config.port}`);
