2dc0764274361193c886c8920c90a0aa30326cf4
Victor BURIDARD <vburidar@e1r3p9.42.fr>
2020-02-21 13:30:18 +0100

    divide populate between service and model

---
+++ src/models/populate.js
@@ -0,0 +1,78 @@
+import PostgresService from '../services/postgres';
+import { interests } from '../../populate_data/populate';
+
+export default class Populate {
+  static async insertInterests() {
+    Object.keys(interests).forEach(async (key) => {
+      await PostgresService.query('INSERT INTO interests (name) VALUES ($1)', [interests[key]]);
+    });
+  }
+
+  static async getSuggestionList(userLogin) {
+    const list = await PostgresService.query(`
+    WITH virtual_user AS (
+        SELECT 
+        users.id,
+        EXTRACT (YEAR FROM AGE(users.birthdate)) AS age,
+        sexual_preference,
+        gender,
+        latitude,
+        longitude
+    FROM (SELECT * FROM users WHERE login = $1) AS users
+    INNER JOIN locations ON users.id = locations.user_id),
+    virtual_interests AS (
+    SELECT * FROM ( SELECT * FROM users_interests WHERE user_id = (SELECT id FROM virtual_user)) AS interests),
+    nb_interest AS (
+    SELECT COUNT(*) AS value FROM users_interests WHERE user_id = (SELECT id FROM virtual_user))
+  
+    SELECT
+      interests.user_id, 
+      interests.common_interests, 
+      locations.distance,
+      users.gender AS gender_receiver,
+      users.sexual_preference::bit(4) AS pref_receiver,
+      (SELECT gender FROM virtual_user)::int as gender_sender,
+      (SELECT sexual_preference FROM virtual_user)::int as pref_sender,
+      abs((SELECT age FROM virtual_user) - EXTRACT (YEAR FROM AGE(users.birthdate))) as age_difference,
+      log(1 + (exp(1) - 1) * (common_interests::float / (SELECT value FROM nb_interest))) AS score_interest,
+      1 / (exp(abs((SELECT age FROM virtual_user) - EXTRACT (YEAR FROM AGE(users.birthdate)))/10)) as score_age,
+      1 / (exp(distance / 10)) AS score_distance,
+      log(1 + 1.7 * (common_interests::float / (SELECT value FROM nb_interest))) + 1 / (exp(abs((SELECT age FROM virtual_user) - EXTRACT (YEAR FROM AGE(users.birthdate)))/10)) + 1 / (exp(distance / 10)) AS score
+    FROM users
+
+    INNER JOIN
+      (SELECT
+        user_id, 
+        111 * |/((latitude - (SELECT latitude FROM virtual_user))^2 + (longitude - (SELECT longitude FROM virtual_user))^2) AS distance 
+      FROM locations) AS locations
+    ON locations.user_id = users.id
+
+    INNER JOIN
+      (SELECT
+        user_id,
+        COUNT (*) AS common_interests
+      FROM (SELECT  * FROM users_interests WHERE user_id != (SELECT id FROM virtual_user)) AS test1
+
+      INNER JOIN
+      (SELECT interest_id FROM users_interests WHERE user_id = (SELECT id FROM virtual_user)) AS test2
+      ON test1.interest_id = test2.interest_id
+      GROUP BY user_id) AS interests
+
+    ON locations.user_id = interests.user_id
+
+    WHERE locations.distance < 20
+    AND (SELECT sexual_preference FROM virtual_user) & users.gender != 0
+    AND (SELECT gender FROM virtual_user) & users.sexual_preference != 0
+    ORDER BY score DESC`, [userLogin]);
+    return (list);
+  }
+
+  static async createLike(visitorLogin, visitedId) {
+    const like = await PostgresService.query(`
+      WITH virtual_user AS(
+        SELECT id FROM users WHERE login = $1
+      )
+      INSERT INTO likes (receiver_id, sender_id) VALUES ((SELECT id FROM virtual_user) , $2)`, [visitorLogin, visitedId]);
+    return (like);
+  }
+}
--- src/services/populate.js
+++ src/services/populate.js
@@ -1,10 +1,9 @@
-import Event from '../models/Event';
-import { ErrException } from '../api/middlewares/errorHandler';
 import {
   names, firstNamesWoman, firstNamesMan, interests, locations,
 } from '../../populate_data/populate';
 import AuthService from './auth';
 import PostgresService from './postgres';
+import Populate from '../models/populate';
 
 export default class PopulateService {
   static randomDate(dateStart, dateEnd) {
@@ -230,63 +229,7 @@ export default class PopulateService {
   }
 
   static async generateLike(userLogin) {
-    const list = await PostgresService.query(`
-    WITH virtual_user AS (
-      SELECT 
-        users.id,
-        EXTRACT (YEAR FROM AGE(users.birthdate)) AS age,
-        sexual_preference,
-        gender,
-        latitude,
-        longitude
-      FROM (SELECT * FROM users WHERE login = $1) AS users
-      INNER JOIN locations ON users.id = locations.user_id),
-    virtual_interests AS (
-      SELECT * FROM ( SELECT * FROM users_interests WHERE user_id = (SELECT id FROM virtual_user)) AS interests),
-    nb_interest AS (
-      SELECT COUNT(*) AS value FROM users_interests WHERE user_id = (SELECT id FROM virtual_user))
-      
-      SELECT
-      interests.user_id, 
-      interests.common_interests, 
-      locations.distance,
-      users.gender AS gender_receiver,
-      users.sexual_preference::bit(4) AS pref_receiver,
-      (SELECT gender FROM virtual_user)::int as gender_sender,
-      (SELECT sexual_preference FROM virtual_user)::int as pref_sender,
-      abs((SELECT age FROM virtual_user) - EXTRACT (YEAR FROM AGE(users.birthdate))) as age_difference,
-      log(1 + (exp(1) - 1) * (common_interests::float / (SELECT value FROM nb_interest))) AS score_interest,
-      1 / (exp(abs((SELECT age FROM virtual_user) - EXTRACT (YEAR FROM AGE(users.birthdate)))/10)) as score_age,
-      1 / (exp(distance / 10)) AS score_distance,
-      log(1 + 1.7 * (common_interests::float / (SELECT value FROM nb_interest))) + 1 / (exp(abs((SELECT age FROM virtual_user) - EXTRACT (YEAR FROM AGE(users.birthdate)))/10)) + 1 / (exp(distance / 10)) AS score
-    FROM users
-    
-    INNER JOIN
-      (SELECT
-        user_id, 
-        111 * |/((latitude - (SELECT latitude FROM virtual_user))^2 + (longitude - (SELECT longitude FROM virtual_user))^2) AS distance 
-      FROM locations) AS locations
-    ON locations.user_id = users.id
-    
-    INNER JOIN
-      
-      (SELECT
-        user_id,
-        COUNT (*) AS common_interests
-      FROM
-        (SELECT  * FROM users_interests WHERE user_id != (SELECT id FROM virtual_user)) AS test1
-
-      INNER JOIN
-        (SELECT interest_id FROM users_interests WHERE user_id = (SELECT id FROM virtual_user)) AS test2
-      ON test1.interest_id = test2.interest_id
-      GROUP BY user_id) AS interests
-    
-    ON locations.user_id = interests.user_id
-
-    WHERE locations.distance < 20
-    AND (SELECT sexual_preference FROM virtual_user) & users.gender != 0
-    AND (SELECT gender FROM virtual_user) & users.sexual_preference != 0
-    ORDER BY score DESC`, [userLogin]);
+    const list = await Populate.getSuggestionList(userLogin);
     const arr = [];
     const nbLikeSent = Math.ceil(list.rows.length / 10);
     while (arr.length < nbLikeSent) {
@@ -294,21 +237,15 @@ export default class PopulateService {
       if (arr.indexOf(r) === -1) arr.push(r);
     }
     arr.map(async (elem) => {
-      await PostgresService.query(`
-      WITH virtual_user AS(
-        SELECT id FROM users WHERE login = $1
-      )
-      INSERT INTO likes (receiver_id, sender_id) VALUES ((SELECT id FROM virtual_user) , $2)`, [userLogin, list.rows[elem].user_id]);
+      await Populate.createLike(userLogin, list.rows[elem].user_id);
     });
     return (null);
   }
 
   static async populate() {
-    Object.keys(interests).forEach(async (key) => {
-      await PostgresService.query('INSERT INTO interests (name) VALUES ($1)', [interests[key]]);
-    });
-    const nUsers = 10000;
-    const partitionSize = 1000;
+    await Populate.insertInterests();
+    const nUsers = 1000;
+    const partitionSize = 100;
     let nPartition = 0;
     const tabUser = this.createNUsers(nUsers);
     let cmp = 0;
